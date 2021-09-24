72d574126ea170e7b17e5e821fbd6f5e96bbaff1
Dipo <adanikindipo@gmail.com>
2021-09-25 00:14:41 +0100

    add paragraph for 'liked by' comment

--- src/components/DisplayMovies.js
+++ src/components/DisplayMovies.js
@@ -3,18 +3,21 @@ import React, { Component } from "react";
 class DisplayMovies extends Component {
   render() {
     const { moviesArr } = this.props;
-    console.log(moviesArr);
+
     return (
       <div>
         {moviesArr.map((movie) => (
           <div key={movie.id}>
             <h2>{movie.name}</h2>
             {movie.favMovies.length > 0 ? (
-              <ul>
-                {movie.favMovies.map((user) => (
-                  <li key={user}>{user}</li>
-                ))}
-              </ul>
+              <div>
+                <p>Liked by:</p>
+                <ul>
+                  {movie.favMovies.map((user) => (
+                    <li key={user}>{user}</li>
+                  ))}
+                </ul>
+              </div>
             ) : (
               <p>None of the current users liked this movie</p>
             )}
